553bcae8fcf27f1d333f481564d4ab98370a29ba
SergeyYakimov <teriertis@yandex.ru>
2020-06-20 01:15:35 +0800

    Made home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { PostPageComponent } from './post-page/post-page.component';
     MainLayoutComponent,
     HomePageComponent,
     PostPageComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
